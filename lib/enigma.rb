34f498fb0e15c5b0ebc1149732faaf8050bc6689
Tara Craig <tara.craig7@gmail.com>
2018-05-11 18:09:37 -0600

    Add get rotations and test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,21 @@
 class Enigma
-  attr_reader   :key
-
-  def new_key
-    @key = [1,2,3,4,5].shuffle
-  end
 
   def offset_digits
     time = Time.new.strftime("%m%e%y").to_i
     digits_arr = (time ** 2 %10_000).to_s.split("")
     digits_arr.map { |num| num.to_i }
+  end
+
+  def get_key
+    ("1" .. "5").to_a.shuffle
+  end
+
+  def get_rotations
+    rotations = []
+    key = get_key
+    rotations << (key[0] + key[1]).to_i
+    rotations << (key[1] + key[2]).to_i
+    rotations << (key[2] + key[3]).to_i
+    rotations << (key[3] + key[4]).to_i
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,18 +13,26 @@ class EnigmaTest < Minitest::Test
 
   def test_new_key
     enigma = Enigma.new
-    enigma.new_key
 
-    assert_equal 1, enigma.key.count(1)
-    assert_equal 1, enigma.key.count(2)
-    assert_equal 1, enigma.key.count(3)
-    assert_equal 1, enigma.key.count(4)
-    assert_equal 1, enigma.key.count(5)
+    assert_equal 1, enigma.get_key.count("1")
+    assert_equal 1, enigma.get_key.count("2")
+    assert_equal 1, enigma.get_key.count("3")
+    assert_equal 1, enigma.get_key.count("4")
+    assert_equal 1, enigma.get_key.count("5")
   end
 
   def test_it_returns_offset_digits
     enigma = Enigma.new
-    actual = enigma.offset_digits
-    expected = [9, 9, 2, 4]
+
+    assert_equal 4, enigma.offset_digits.length
+    assert_equal Integer, enigma.offset_digits.sample.class
+    assert_equal Array, enigma.offset_digits.class
+  end
+
+  def test_it_gets_rotations
+    enigma = Enigma.new
+    assert_equal 4, enigma.get_rotations.length
+    assert_equal Integer, enigma.get_rotations.sample.class
+    assert_equal Array, enigma.get_rotations.class
   end
 end
