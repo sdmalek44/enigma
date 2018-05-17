2dc0a9d073a59fc4e4b40b106c425eb809d90540
Tara Craig <tara.craig7@gmail.com>
2018-05-16 19:18:28 -0600

    Run rubocop on offset calculator test file, fix offenses

--- test/offset_calculator_test.rb
+++ test/offset_calculator_test.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/offset_calculator'
 
 class OffsetCalculatorTest < Minitest::Test
-
-
   def test_todays_date_is_correct_format
     offset_calculator = OffsetCalculator.new
 
@@ -15,7 +15,7 @@ class OffsetCalculatorTest < Minitest::Test
   def test_get_offsets_with_passed_in_date
     offset_calculator = OffsetCalculator.new
     expected = [9, 9, 2, 4]
-    actual = offset_calculator.get_offsets("051118")
+    actual = offset_calculator.get_offsets('051118')
 
     assert_equal expected, actual
   end
